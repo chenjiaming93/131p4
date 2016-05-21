270d9b44702c7120d798c7c30191b6e2bf208849
jic096@ucsd.edu <jic096@ucsd.edu>
2016-05-21 02:59:17 -0700

    vardecl

--- symtable.cc
+++ symtable.cc
@@ -3,3 +3,46 @@
  *
  */
 
+#include "symtable.h"
+
+SymbolTable::SymbolTable() {
+  symbolTable = new vector<map<string, Decl*> >();
+  scopeStack = new vector<scope>();
+  currScope = 0;
+}
+
+void SymbolTable::Push(scope sp) {
+  map<string, Decl*> newScope = map<string, Decl*>();;
+  symbolTable->push_back(newScope);
+  scopeStack->push_back(sp);
+  currScope++;
+}
+
+void SymbolTable::Pop() {
+  symbolTable->pop_back();
+  scopeStack->pop_back();
+  currScope--;
+}
+
+void SymbolTable::AddSymbol(string id, Decl *decl) {
+  if(LookUpCurr(id) != NULL)
+    ReportError::DeclConflict(decl, LookUpCurr(id));
+  symbolTable->at(currScope).insert(pair<string, Decl*>(id, decl));
+}
+
+Decl *SymbolTable::LookUpCurr(string id) {
+  int cnt = symbolTable->at(currScope).count(id);
+  if (cnt > 0)
+    return symbolTable->at(currScope).at(id);
+  return NULL;
+}
+
+Decl *SymbolTable::LookUpScope(string id) {
+  int cnt = 0;
+  for(int i = currScope; i >= 0; i--) {
+    cnt = symbolTable->at(i).count(id);
+    if(cnt > 0)
+      return symbolTable->at(i).at(id);
+  }
+  return NULL;
+}
--- symtable.h
+++ symtable.h
@@ -4,3 +4,46 @@
  *  Header file for Symbol table implementation.
  */
 
+#ifndef _H_symtable
+#define _H_symtable
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <vector>
+#include <map>
+#include <iostream>
+#include "location.h"
+#include "ast_decl.h"
+#include "errors.h"
+
+using namespace std;
+
+class SymbolTable {
+
+  public:
+    typedef enum {
+      Program,
+      Function,
+      Loop,
+      Block,
+      Conditional,
+      Switch
+    } scope;
+
+    SymbolTable();
+    void Push(scope sp);
+    void Pop();
+    void AddSymbol(string id, Decl *decl);
+    Decl *LookUpCurr(string id);
+    Decl *LookUpScope(string id);
+
+    vector<scope> *scopeStack;
+    vector<map<string, Decl*> > *symbolTable;
+    int currScope;
+    bool returned;
+    FnDecl *lastFun;
+
+};
+
+#endif
